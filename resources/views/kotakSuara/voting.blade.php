19e8260f6c3d98d6f6f40eb71a2bf45997a8b68c
arditya17 <vikha.arditya@students.amikom.ac.id>
2020-06-24 14:47:29 +0700

    test

--- resources/views/kotakSuara/voting.blade.php
+++ resources/views/kotakSuara/voting.blade.php
@@ -5,7 +5,7 @@
 <div class="row justify-content-center">
 	<div class="col-md-10">
 		<div class="card">
-			<div class="card-header">Dashboard</div>
+			<!-- <div class="card-header">Skuy Coblos</div> -->
 
 			<div class="card-body text-center">
 				@if (session('status'))
@@ -14,17 +14,17 @@
 				</div>
 				@endif
 
-				Silakan  Mencoblos..!!!
+				<h1> Silakan  Mencoblos..!!! </h1>
 
 				<div class="row justify-content-center">
 				@foreach($calon as $p)
 				<div class="col-md-6">
 				<form action="/pemilih/vote" method="post"  enctype="multipart/form-data">
 					{{ csrf_field() }}
-							<button type="submit" class="btn btn-block  btn-flat mg-100">
+							<button type="submit" class="btn btn-block btn-outline-primary btn-flat mg-100">
 								<input type="hidden" name="id" value="{{$p->id}}">
 								<input type="hidden" name="suara" value="{{ Auth::user()->id }}">
-							<img src="/img/foto_kades/{{ $p->foto }}" style="width: 400px;height: 300px" class="img-thumbnail" alt="Product Image">
+							<img src="/img/foto_kades/{{ $p->foto }}" style="100%" class="img-thumbnail" alt="Product Image">
 							<input type="hidden" name="jumlah_suara" value="1">
 							</button>
 				</form>
